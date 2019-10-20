c5ef5670d88203d4bf969b878d4f5cb9cc4ffac6
Willie Chalmers III <willieechalmers@gmail.com>
2019-10-20 08:11:55 -0500

    Update Auth.vue to use use firebase-init function

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -8,7 +8,7 @@
       <input type="text" placeholder="Password" v-model="password" />
       <br />
       <br />
-      <button v-if="inSignUpMode" v-on:click="signup">Sign Up</button>
+      <button v-if="inSignUpMode" v-on:click="signUp">Sign Up</button>
       <button v-else v-on:click="login">Login</button>
       <br /><br />
       <button v-if="inSignUpMode" v-on:click="changeToLogin">Go to Login</button>
@@ -19,20 +19,10 @@
 
 <script>
 import firebase from 'firebase';
+import initializeFirebase from '../api/firebase-init';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCdPGhsho4yax5ev3nE939Xp45gTpLSMLE',
-  authDomain: 'eventually-dev.firebaseapp.com',
-  databaseURL: 'https://eventually-dev.firebaseio.com',
-  projectId: 'eventually-dev',
-  storageBucket: 'eventually-dev.appspot.com',
-  messagingSenderId: '1012667528749',
-  appId: '1:1012667528749:web:a6db81f286ad06133c6aa4',
-  measurementId: 'G-NFTL1BMRC8',
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+initializeFirebase();
+
 export default {
   name: 'auth',
   props: { inSignUpMode: Boolean },
@@ -55,7 +45,7 @@ export default {
         console.error(e);
       }
     },
-    async signup() {
+    async signUp() {
       try {
         const credential = await firebase.auth()
           .createUserWithEmailAndPassword(this.email, this.password);
