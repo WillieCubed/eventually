9653870e34eb582ab9dad80662fc2bee2eba1613
Willie Chalmers III <willieechalmers@gmail.com>
2019-10-20 08:15:41 -0500

    Add TaskBuffetSheet to Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,23 @@
 <template>
   <main>
+    <task-buffet-sheet :tasks="tasks"></task-buffet-sheet>
   </main>
 </template>
 
 <script>
+import TaskBuffetSheet from '../components/TaskBuffetSheet.vue';
 
 export default {
   name: 'home',
+  components: {
+    'task-buffet-sheet': TaskBuffetSheet,
+  },
   data() {
-    return {};
+    return {
+      tasks: [
+        // TODO: Load data from store
+      ],
+    };
   },
 };
 </script>
