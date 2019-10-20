4a10ff41d6f9dbebf13baf494b7b472f00608742
Willie Chalmers III <willieechalmers@gmail.com>
2019-10-20 08:12:23 -0500

    Create more stubs for auth

---
+++ src/api/auth.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+import 'firebase/auth';
+
+const auth = firebase.auth();
+
+async function signInWithEmail(email, password) {
+  // TODO(eric): Move sign-out here
+}
+
+async function signUp() {
+  // TODO(eric): Handle sign-up here
+}
+
+async function signOut() {
+  // TODO(eric): Handle sign-out here
+}
